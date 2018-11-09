485b0b32771381ea97271e962d14017e8aaa18b5
Joakim Carlstein <joakim@aboviq.com>
2018-11-09 13:46:18 +0100

    feat: implement the file collector aka. kollektor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 'use strict';
+const {dirname, basename, relative, resolve} = require('path');
+const readdirRecursive = require('@aboviq/readdir-recursive');
+const {isMatch} = require('matcher');
+
 const isObjectEmpty = obj => Object.keys(obj).length === 0;
 
-const kollektor = ({handlers}) => {
+const validateHandlers = handlers => {
 	if (!handlers) {
 		throw new TypeError('Missing handlers option');
 	}
@@ -11,6 +15,48 @@ const kollektor = ({handlers}) => {
 	if (isObjectEmpty(handlers)) {
 		throw new TypeError('The handlers option must contain at least one handler');
 	}
+	Object.entries(handlers).forEach(([filename, handler]) => {
+		if (typeof handler !== 'function') {
+			throw new TypeError(`The handler for "${filename}" is not a function`);
+		}
+	});
+};
+
+const getMatchingFiles = (dir, filePatterns) =>
+	readdirRecursive(dir, {
+		filter: ({file}) => filePatterns.some(filePattern => isMatch(file, filePattern))
+	});
+
+const kollektor = async ({handlers, cwd = process.cwd()}) => {
+	validateHandlers(handlers);
+
+	const filePatterns = Object.keys(handlers);
+	const paths = await getMatchingFiles(resolve(cwd), filePatterns);
+
+	paths.sort();
+
+	const fileMap = new Map();
+
+	for (const fullPath of paths) {
+		const path = relative(resolve(cwd), fullPath);
+		const dirPath = dirname(fullPath);
+		const dir = dirname(path);
+		const file = basename(path);
+
+		let data = fileMap.get(dir) || {dir, dirPath};
+
+		for (const [filePattern, handler] of Object.entries(handlers)) {
+			if (isMatch(file, filePattern)) {
+				// eslint-disable-next-line no-await-in-loop
+				const result = await handler(fullPath, data);
+				data = {...data, ...result};
+			}
+		}
+
+		fileMap.set(dir, data);
+	}
+
+	return [...fileMap.values()];
 };
 
 module.exports = kollektor;
--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,223 @@
 'use strict';
+const {promisify} = require('util');
+const {resolve} = require('path');
+const {readFile} = require('fs');
 const kollektor = require('.');
 
+const readFileAsync = promisify(readFile);
+
 describe('kollektor', () => {
-	it('throws without options', () => {
-		expect(kollektor).toThrowError(TypeError);
+	it('throws without options', async () => {
+		await expect(kollektor()).rejects.toThrowError(TypeError);
 	});
 
-	it('throws without handlers', () => {
-		const kollekt = () => kollektor({});
+	it('throws without handlers', async () => {
+		const result = kollektor({});
 
-		expect(kollekt).toThrowError(TypeError);
-		expect(kollekt).toThrowError('Missing handlers option');
+		await expect(result).rejects.toThrowError(TypeError);
+		await expect(result).rejects.toThrowError('Missing handlers option');
 	});
 
-	it('throws when handlers is not an object', () => {
-		const kollekt = () => kollektor({handlers: 'here are my handlers'});
+	it('throws when handlers is not an object', async () => {
+		const result = kollektor({handlers: 'here are my handlers'});
 
-		expect(kollekt).toThrowError(TypeError);
-		expect(kollekt).toThrowError('The handlers option must be an object');
+		await expect(result).rejects.toThrowError(TypeError);
+		await expect(result).rejects.toThrowError('The handlers option must be an object');
 	});
 
-	it('throws when handlers is an empty object', () => {
-		const kollekt = () => kollektor({handlers: {}});
+	it('throws when handlers is an empty object', async () => {
+		const result = kollektor({handlers: {}});
 
-		expect(kollekt).toThrowError(TypeError);
-		expect(kollekt).toThrowError('The handlers option must contain at least one handler');
+		await expect(result).rejects.toThrowError(TypeError);
+		await expect(result).rejects.toThrowError(
+			'The handlers option must contain at least one handler'
+		);
+	});
+
+	it('throws when any handler is not a function', async () => {
+		const result = kollektor({handlers: {filename1: () => {}, filename2: 'not a function'}});
+
+		await expect(result).rejects.toThrowError(TypeError);
+		await expect(result).rejects.toThrowError('The handler for "filename2" is not a function');
+	});
+
+	it('calls the given handler for matching files', async () => {
+		const packageJsonHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'package.json': packageJsonHandler
+			}
+		});
+
+		expect(packageJsonHandler).toHaveBeenCalled();
+	});
+
+	it('does not call the given handler when no matching file exists', async () => {
+		const pkgJsonHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'pkg.json': pkgJsonHandler
+			}
+		});
+
+		expect(pkgJsonHandler).not.toHaveBeenCalled();
+	});
+
+	it('calls the given handler with the full path to the handled file', async () => {
+		const packageJsonHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'package.json': packageJsonHandler
+			}
+		});
+
+		expect(packageJsonHandler).toHaveBeenCalledWith(
+			resolve(__dirname, '..', 'package.json'),
+			expect.anything()
+		);
+	});
+
+	it("calls the given handler with data.dir as the handled file's dir relative to cwd", async () => {
+		const packageJsonHandler = jest.fn();
+		const indexJsHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'package.json': packageJsonHandler,
+				'index.js': indexJsHandler
+			}
+		});
+
+		expect(packageJsonHandler).toHaveBeenCalledWith(
+			expect.anything(),
+			expect.objectContaining({dir: '.'})
+		);
+		expect(indexJsHandler).toHaveBeenCalledWith(
+			expect.anything(),
+			expect.objectContaining({dir: 'src'})
+		);
+	});
+
+	it("calls the given handler with data.dirPath as the handled file's dir's full path", async () => {
+		const packageJsonHandler = jest.fn();
+		const indexJsHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'package.json': packageJsonHandler,
+				'index.js': indexJsHandler
+			}
+		});
+
+		expect(packageJsonHandler).toHaveBeenCalledWith(
+			expect.anything(),
+			expect.objectContaining({dirPath: resolve(__dirname, '..')})
+		);
+		expect(indexJsHandler).toHaveBeenCalledWith(
+			expect.anything(),
+			expect.objectContaining({dirPath: __dirname})
+		);
+	});
+
+	it('sets data.dir correctly when cwd option is set', async () => {
+		const indexJsHandler = jest.fn();
+
+		await kollektor({
+			cwd: __dirname,
+			handlers: {
+				'index.js': indexJsHandler
+			}
+		});
+
+		expect(indexJsHandler).toHaveBeenCalledWith(
+			expect.anything(),
+			expect.objectContaining({dir: '.'})
+		);
+	});
+
+	it('can handle generic file handlers', async () => {
+		const jsHandler = jest.fn();
+
+		await kollektor({
+			handlers: {
+				'*.js': jsHandler
+			}
+		});
+
+		expect(jsHandler.mock.calls).toEqual(
+			expect.arrayContaining([
+				[expect.stringMatching(/\/commitlint\.config\.js$/), expect.objectContaining({dir: '.'})],
+				[expect.stringMatching(/\/index\.js$/), expect.objectContaining({dir: 'src'})],
+				[expect.stringMatching(/\/index\.test\.js$/), expect.objectContaining({dir: 'src'})]
+			])
+		);
+	});
+
+	it('returns an array with one item for each unique folder', async () => {
+		const result = await kollektor({
+			cwd: resolve(__dirname, '..', 'fixtures'),
+			handlers: {
+				'config.yml': () => {},
+				'data.json': () => {}
+			}
+		});
+
+		expect(result).toHaveLength(2);
+	});
+
+	it('merges the result of each handler with the result for that folder', async () => {
+		const result = await kollektor({
+			cwd: resolve(__dirname, '..', 'fixtures'),
+			handlers: {
+				'config.yml': () => ({config: true}),
+				'data.json': () => ({data: true})
+			}
+		});
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({dir: 'another', config: true, data: true}),
+				expect.objectContaining({dir: 'subfolder', config: true, data: true})
+			])
+		);
+	});
+
+	it('can handle async handlers', async () => {
+		const result = await kollektor({
+			cwd: resolve(__dirname, '..', 'fixtures'),
+			handlers: {
+				'config.yml': async configFile => {
+					const ymlContent = await readFileAsync(configFile, 'utf8');
+					return {config: ymlContent.trim()};
+				}
+			}
+		});
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({dir: 'another', config: 'hello: earth'}),
+				expect.objectContaining({dir: 'subfolder', config: 'hello: world'})
+			])
+		);
+	});
+
+	it('runs handlers in alphabetical order according to matching filename', async () => {
+		const result = await kollektor({
+			cwd: resolve(__dirname, '..', 'fixtures'),
+			handlers: {
+				'config.yml': () => ({value: 1}),
+				'data.json': (_, {value}) => ({value: value + 1})
+			}
+		});
+
+		expect(result).toEqual(
+			expect.arrayContaining([
+				expect.objectContaining({dir: 'another', value: 2}),
+				expect.objectContaining({dir: 'subfolder', value: 2})
+			])
+		);
 	});
 });
